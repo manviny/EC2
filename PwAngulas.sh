db6a4d702b6ca10ad3fc3a4242273d3c9551a646
manviny <manol@indinet.es>
2016-02-14 20:02:46 +0100

    Update PwAngulas.sh

--- PwAngulas.sh
+++ PwAngulas.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-cd
-sudo rm borraPW.sh creaPW.sh finalizaPW.sh
+# comprubea parametro 
+if [ -n "$1" ]
+then
+    echo "Cargando..."
+else 
+    echo "Saliendo, falta nombre de la web"
+    exit
+fi
+
+cd /opt/bitnami/apps/$1/htdocs/site/modules
+rm -rf ./PwAngular
+mkdir PwAngular 
 wget https://github.com/manviny/manviny.PwAngular/archive/master.zip  
+unzip master.zip
 
 
+echo "Se han instalado PwAngular"
 
 
-&& sudo chmod +x creaPW.sh
-wget https://raw.githubusercontent.com/manviny/EC2/master/finalizaPW.sh  && sudo chmod +x finalizaPW.sh
-wget https://raw.githubusercontent.com/manviny/EC2/master/borraPW.sh  && sudo chmod +x borraPW.sh
-sudo rm ./PwScripts.sh
-echo "Se han instalado los scripts creaWeb.sh, borraPw.sh y finalizaPW.sh"
-
-https://github.com/manviny/manviny.PwAngular/archive/master.zip
