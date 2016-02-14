988f9483027e0c1d6e28f4207c1aea576964fbd4
manviny <manol@indinet.es>
2016-02-14 17:41:43 +0100

    Update finalizaPW.sh

--- finalizaPW.sh
+++ finalizaPW.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/bitnami/apps/$1/htdocs
-sudo chmod 644  ./site/config.php
+sudo chmod 655  ./site/config.php
 sudo find . -type d -exec chmod 775 {} \;
 sudo find . -type f -exec chmod 664 {} \;
 sudo chown -R bitnami:daemon ./*
