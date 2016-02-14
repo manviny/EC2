e44dd896ef6249288b8b61fe150ddc39a9389c60
manviny <manol@indinet.es>
2016-02-14 19:58:59 +0100

    Update PwAngulas.sh

--- PwAngulas.sh
+++ PwAngulas.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+cd
+sudo rm borraPW.sh creaPW.sh finalizaPW.sh
+wget https://github.com/manviny/manviny.PwAngular/archive/master.zip  
+
+
+
+
+&& sudo chmod +x creaPW.sh
+wget https://raw.githubusercontent.com/manviny/EC2/master/finalizaPW.sh  && sudo chmod +x finalizaPW.sh
+wget https://raw.githubusercontent.com/manviny/EC2/master/borraPW.sh  && sudo chmod +x borraPW.sh
+sudo rm ./PwScripts.sh
+echo "Se han instalado los scripts creaWeb.sh, borraPw.sh y finalizaPW.sh"
+
 https://github.com/manviny/manviny.PwAngular/archive/master.zip
