8b3b2308d586b2e694d8eab5df5455c8151dd160
manviny <manol@indinet.es>
2016-02-14 17:17:21 +0100

    Update PwScripts.sh

--- PwScripts.sh
+++ PwScripts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd
+sudo rm borraPW.sh creaPW.sh finalizaPW.sh
 wget https://raw.githubusercontent.com/manviny/EC2/master/creaPW.sh  && sudo chmod +x creaPW.sh
 wget https://raw.githubusercontent.com/manviny/EC2/master/finalizaPW.sh  && sudo chmod +x finalizaPW.sh
 wget https://raw.githubusercontent.com/manviny/EC2/master/borraPW.sh  && sudo chmod +x borraPW.sh
