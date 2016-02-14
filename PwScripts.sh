15f79274466580afa5018c3afc3e9548ecaff9a5
manviny <manol@indinet.es>
2016-02-14 16:53:03 +0100

    Create PwScripts.sh

---
+++ PwScripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+$ wget https://gist.githubusercontent.com/manviny/459d7fd7e234540a0786/raw/cffe427cb1211b5907fa4a7fd5825fa8f21f657e/creaPW.sh  && sudo chmod +x creaPW.sh
+$ wget https://gist.githubusercontent.com/manviny/459d7fd7e234540a0786/raw/cffe427cb1211b5907fa4a7fd5825fa8f21f657e/finaliza.sh  && sudo chmod +x finaliza.sh
+$ wget https://gist.githubusercontent.com/manviny/459d7fd7e234540a0786/raw/cffe427cb1211b5907fa4a7fd5825fa8f21f657e/borraPW.sh  && sudo chmod +x borraPW.sh
+echo "Se han instalado los scripts creaWeb.sh, borraPw.sh y finalizaPW.sh"
