087f0f49b7170248478de72c8e91ab6b3e0ca08c
manviny <manol@indinet.es>
2016-02-14 17:46:44 +0100

    Update finalizaPW.sh

--- finalizaPW.sh
+++ finalizaPW.sh
@@ -12,4 +12,5 @@ sudo find . -type d -exec chmod 775 {} \;
 sudo find . -type f -exec chmod 664 {} \;
 sudo chown -R bitnami:daemon ./*
 sudo rm -rf site-beginner  site-classic  site-default  site-languages ProcessWire-master
+sudo rm -rf ./site/install
 sudo rm master.zip install.php
