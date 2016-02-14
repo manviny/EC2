f0eb94e1c21a66029affe04d312668c059f92eae
manviny <manol@indinet.es>
2016-02-14 17:02:39 +0100

    Update PwScripts.sh

--- PwScripts.sh
+++ PwScripts.sh
@@ -3,4 +3,5 @@
 wget https://raw.githubusercontent.com/manviny/EC2/master/creaPW.sh  && sudo chmod +x creaPW.sh
 wget https://raw.githubusercontent.com/manviny/EC2/master/finalizaPW.sh  && sudo chmod +x finalizaPW.sh
 wget https://raw.githubusercontent.com/manviny/EC2/master/borraPW.sh  && sudo chmod +x borraPW.sh
+sudo rm ./PwScripts.sh
 echo "Se han instalado los scripts creaWeb.sh, borraPw.sh y finalizaPW.sh"
