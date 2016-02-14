07ef7777c8fba36324e64d2e70504af85ef6edbc
manviny <manol@indinet.es>
2016-02-14 16:54:29 +0100

    Create creaPW.sh

---
+++ creaPW.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# creaweb nombreweb dbpass 
+
+
+
+# 1.- Crea estructura de directorios
+sudo -u $USER mkdir -p /opt/bitnami/apps/$1/htdocs /opt/bitnami/apps/$1/conf
+
+
+# 2.- Permisos archivos y directorios para el usuario bitnami
+sudo -u $USER chown -R bitnami /opt/bitnami/apps/$1
+
+# 3.- descargar y descomprimir processwire
+cd /opt/bitnami/apps/$1/htdocs
+wget https://github.com/ryancramerdesign/ProcessWire/archive/master.zip
+sudo unzip master.zip
+sudo mv ProcessWire-master/* ./
+sudo mv site-blank site
+# 3,5.- permisos para instalar 
+sudo chmod 777 -R ./site/assets ./site/modules
+sudo chmod 777 ./site/config.php
+sudo mv htaccess.txt .htaccess
+
+# 3.- descomprimir zip de la app, sino exite crea un phpinfo como 
+
+#if [ -n "$3" ]
+#then
+#     sudo unzip ./$3 /opt/bitnami/apps/$1/htdocs/
+#else 
+
+#    sudo -u $USER echo '<?php phpinfo(); ?>' > /opt/bitnami/apps/$1/htdocs/index.php    
+#fi
+
+
+
+
+# 4.- Crea configuración para acceder a la web http://mibitnami.com/miweb
+sudo -u $USER cat > /opt/bitnami/apps/$1/conf/$1.conf <<EOL
+Alias /$1/ "/opt/bitnami/apps/$1/htdocs/"
+Alias /$1 "/opt/bitnami/apps/$1/htdocs"
+<Directory "/opt/bitnami/apps/$1/htdocs">
+    Options Indexes MultiViews
+    AllowOverride All
+    <IfVersion < 2.3 >
+    Order allow,deny
+    Allow from all
+    </IfVersion>
+    <IfVersion >= 2.3>
+    Require all granted
+    </IfVersion>
+</Directory>
+EOL
+
+
+# 5.- Añadir en la útima linea  de la configuración de apache la nueva entrada
+echo Include "/opt/bitnami/apps/$1/conf/$1.conf" >> /opt/bitnami/apache2/conf/httpd.conf
+
+
+# 6.- Reinicializar servicio apache
+/opt/bitnami/ctlscript.sh restart apache
+
+# 7.- lista webs disponibles
+#ls -ls /opt/bitnami/apps
+
+# 8.- Crear Base de datos
+echo "Tu usuario es: "$1
+echo "El nombre de la base de datos es: "$1
+pass=`tr -dc A-Za-z0-9 < /dev/urandom | head -c 8 | xargs`
+echo "El password de la base de datos es: " $pass
+
+mysql -u root -p$2 << EOF
+CREATE DATABASE IF NOT EXISTS $1;
+GRANT USAGE ON *.* TO $1@localhost IDENTIFIED BY '$pass';
+GRANT ALL PRIVILEGES ON $1.* TO $1@localhost;
+FLUSH PRIVILEGES;
+EOF
