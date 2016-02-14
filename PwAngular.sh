c3fa3c9a0df177e33722243c1c5e78bb78e955a8
manviny <manol@indinet.es>
2016-02-14 20:05:31 +0100

    Create PwAngular.sh

---
+++ PwAngular.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# comprubea parametro 
+if [ -n "$1" ]
+then
+    echo "Cargando..."
+else 
+    echo "Saliendo, falta nombre de la web"
+    exit
+fi
+
+cd /opt/bitnami/apps/$1/htdocs/site/modules
+rm -rf ./PwAngular
+mkdir PwAngular 
+cd PwAngular
+wget https://github.com/manviny/manviny.PwAngular/archive/master.zip  
+unzip master.zip
+sudo mv manviny.PwAngular-master/* ./
+rm master.zip
+
+
+echo "Se han instalado PwAngular"
