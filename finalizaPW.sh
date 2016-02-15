d599d26f0b32f86676857889ce4cfcfc28623535
manviny <manol@indinet.es>
2016-02-15 17:46:46 +0100

    Update finalizaPW.sh

--- finalizaPW.sh
+++ finalizaPW.sh
@@ -11,6 +11,6 @@ sudo chmod 655  ./site/config.php
 sudo find . -type d -exec chmod 775 {} \;
 sudo find . -type f -exec chmod 664 {} \;
 sudo chown -R bitnami:daemon ./*
-sudo rm -rf site-beginner  site-classic  site-default  site-languages ProcessWire-master
+sudo rm -rf site-beginner  site-classic  site-blank  site-languages ProcessWire-master
 sudo rm -rf ./site/install
 sudo rm master.zip install.php
