49a673e44316b5db8df3598452d5320a67dc7be8
manviny <manol@indinet.es>
2016-02-23 17:19:40 +0100

    Create api.php

---
+++ api.php
@@ -0,0 +1,35 @@
+```php
+<?php 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Authorization, X-Requested-With, Content-Type, Origin, Accept");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 3600');
+
+
+	//////////////////////////////////////////////////////////////
+	// 					Get post Data
+	//////////////////////////////////////////////////////////////
+
+	$request = file_get_contents('php://input');
+	$param = json_decode($request,true);
+
+
+	switch ( $input->urlSegment1 ) {
+	    case "enviaemail":	enviaEmail( $param['user'], $param['pass']); break;
+
+		default:
+			throw new Wire404Exception();
+	} 
+
+	/**
+	 * Poperties:
+	 * Key, LastModified, ETag, Size, StorageClass, Owner.DisplayName, Owner.ID
+	 *
+	 */
+	function enviaEmail( $user,  $pass) { 
+		echo $user."H oi ".$pass;
+
+		return;
+	}	
+```
